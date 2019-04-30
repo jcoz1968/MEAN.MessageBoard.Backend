b8dd3b6efc76ddfc05b7e2852757af739f86c4e8
Jeffery Cosby <jcoz1968@gmail.com>
2019-04-30 15:52:23 -0500

    wire up mongo

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+const url = 'mongodb://localhost:27017';
+const dbName = 'messageBoard';
+let db;
+
+app.use(bodyParser.text());
+app.use(cors());
+
+app.post('/api/message', (req, res) => { 
+  // console.log(req.body);
+  db.collection('messages').insertOne({
+    msg: req.body
+  });  
+  res.status(200).send();
+});
+
+MongoClient.connect(url, (err, client) => { 
+  if (err) {
+    return console.log('mongodb error', err);
+  }
+  console.log('connected successfully to db server');
+  db = client.db(dbName);
+  db.collection('messages').find({}).toArray((err, docs) => { 
+    console.log(docs);
+  });
+});
+
+app.listen(port, () => {
+  console.log('App running on port: ', port);
+});

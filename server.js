90afd1122a7bef5823a8f2c56eb59a24fbf897a7
Jeffery Cosby <jcoz1968@gmail.com>
2019-04-30 22:17:02 -0500

    get route

--- server.js
+++ server.js
@@ -21,13 +21,21 @@ app.post('/api/message', (req, res) => {
   res.status(200).send();
 });
 
+app.get('/api/message', async (req, res) => { 
+  const docs = await db.collection('messages').find({}).toArray();  
+  if (!docs) {
+    return res.json({ error: 'error getting messages.' });
+  }
+  res.json(docs);
+});
+
 MongoClient.connect(url, (err, client) => { 
   if (err) {
     return console.log('mongodb error', err);
   }
   console.log('connected successfully to db server');
   db = client.db(dbName);
-  getMessages();
+  // getMessages();
 });
 
 async function getMessages() {
